3a769e6a6d2df2cda12a1dbda5791ce27765b04b
metazoan94 <metazoan94@gmail.com>
2024-04-01 22:36:17 +0800

    add trackball, clean up etc.

--- keyboards/dutimm/mcuconf.h
+++ keyboards/dutimm/mcuconf.h
@@ -5,6 +5,8 @@
 #undef RP_I2C_USE_I2C0
 #define RP_I2C_USE_I2C0 TRUE
 
-// #undef RP_SPI_USE_SPI1
-// #define RP_SPI_USE_SPI1 TRUE
+#undef RP_SPI_USE_SPI1
+#define RP_SPI_USE_SPI1 TRUE
 
+#undef RP_PWM_USE_PWM6
+#define RP_PWM_USE_PWM6 TRUE
