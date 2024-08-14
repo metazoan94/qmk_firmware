d6b81a8c886ba5880d692a8b777ce963e1c3719e
metazoan94 <metazoan94@gmail.com>
2024-08-14 15:48:58 +0800

    homerow mod & trackball & rgb

--- keyboards/dutimm/mcuconf.h
+++ keyboards/dutimm/mcuconf.h
@@ -2,11 +2,11 @@
 
 #include_next <mcuconf.h>
 
-#undef RP_I2C_USE_I2C0
-#define RP_I2C_USE_I2C0 TRUE
+// #undef RP_I2C_USE_I2C0
+// #define RP_I2C_USE_I2C0 TRUE
 
-#undef RP_SPI_USE_SPI1
-#define RP_SPI_USE_SPI1 TRUE
+#undef RP_SPI_USE_SPI0
+#define RP_SPI_USE_SPI0 TRUE
 
-#undef RP_PWM_USE_PWM3
-#define RP_PWM_USE_PWM3 TRUE
+#undef RP_PWM_USE_PWM6
+#define RP_PWM_USE_PWM6 TRUE
